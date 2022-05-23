87d2f2ef6d38f8cab1167605d17024b894151bc6
Omar D. Ramirez <omardramirez2002@gmail.com>
2022-05-22 20:42:36 -0500

    Creates the simple architecture for the message room

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import { io } from 'socket.io-client'
 import React, { useState, useEffect } from 'react'
+import Paper from '@mui/material/Paper'
+import TextField from '@mui/material/TextField';
+
 
 const socket = io('http://localhost:3001')
 
@@ -18,12 +21,21 @@ function App() {
     }
 
   return (
-    <>
+    <div className='backdrop'>
+      <Paper elevation={3}>
+        {/* name */}
+        <div></div>
+        {/* chat  */}
+        <Paper></Paper>
+        {/* typing */}
+        <TextField id="standard-basic" label="Standard" variant="standard" />
+        
+      </Paper>
     {
         userCode && 
         <div>{userCode}</div>
     }
-    </>
+    </div>
   );
 }
 
