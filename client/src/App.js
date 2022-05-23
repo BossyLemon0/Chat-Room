b29e56c54613c211ff2d8f0cdf5a29472e01c21a
Omar D. Ramirez <omardramirez2002@gmail.com>
2022-05-22 21:25:58 -0500

    Styles the page a little more

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { io } from 'socket.io-client'
 import React, { useState, useEffect } from 'react'
 import Paper from '@mui/material/Paper'
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 import "./styles/chatroom.css"
 
 const socket = io('http://localhost:3001')
@@ -22,19 +23,30 @@ function App() {
 
   return (
     <div className='backdrop'>
-      <Paper elevation={3}>
+      <Paper elevation={3} sx={{minHeight: 1000, minWidth:800, display:'flex', justifyContent:"center", alignItems:'center', flexDirection:"column"}}>
         {/* name */}
-        <div></div>
-        {/* chat  */}
-        <Paper></Paper>
-        {/* typing */}
-        <TextField id="standard-basic" label="Standard" variant="standard" />
-        
-      </Paper>
-    {
+        <div>
+        {
         userCode && 
         <div>{userCode}</div>
-    }
+        }
+        </div>
+        {/* chat  */}
+        <div className='chatarea'>chat</div>
+        {/* typing */}
+        <div>
+        <TextField
+                  id="standard-basic"
+                  label="message..."
+                  variant="standard"
+                  sx={{minWidth:500}}
+        />
+              <Button variant="text">Send</Button>
+        </div>
+
+        
+      </Paper>
+
     </div>
   );
 }
