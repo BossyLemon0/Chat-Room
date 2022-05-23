d2f34ec573d3bcd68d56b6d96e296b4c9de289ba
Omar D. Ramirez <omardramirez2002@gmail.com>
2022-05-22 23:15:03 -0500

    Creates the logic for rendering the messages

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
 
     const [userCode, setUserCode] = useState(null);
     const [message, setMessage] = useState('');
-    const [sentMessages, setSentMessages] = useState([{}]);
+    const [sentMessages, setSentMessages] = useState([]);
     const handleMessage = (event) => {
       setMessage(event.target.value);
     };
@@ -27,15 +27,54 @@ function App() {
 
     const submit = () => {
       // store the message and change the state of the chat box to add it
-      let date = new Date()
-      let time = date.getTime();
-      setSentMessages(()=>[...sentMessages, {user:userCode,message:message,time:time}])
+      // they are stored in a variable because putting state into state has a delay of one
+      let time = getTime()
+      let currentMsg = getMessage();
+      setSentMessages(()=>[...sentMessages, {user:userCode,message:currentMsg,time}])
       setMessage('')
-      console.log(message)
+
       console.log(sentMessages)
+
+      // testing
+      // let currentMsgArray = getSentMessages();
+      // console.log(currentMsgArray)
     }
 
+    const getTime = ()=>{
+      let date = new Date()
+      var hours = date.getHours();
+      var minutes = date.getMinutes();
+      var ampm = hours >= 12 ? 'pm' : 'am';
+      hours = hours % 12;
+      hours = hours ? hours : 12; // the hour '0' should be '12'
+      minutes = minutes < 10 ? '0'+minutes : minutes;
+      var strTime = hours + ':' + minutes + ' ' + ampm;
+      return strTime;
+    }
 
+    const getMessage = ()=>{
+      return message
+    }
+
+    // const getSentMessages = ()=>{
+    //   return sentMessages
+    // }
+
+    const renderMsgs = (msg) => {
+      // map out sentMessages
+      // if the messages match the persons usercode have the message display right, if not display left
+      
+      if(msg == userCode){
+        return (
+        <Paper></Paper>
+        )
+
+      }
+      else{
+
+      }
+
+    }
     
     function render (){
     }
