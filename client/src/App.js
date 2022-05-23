8563cebd3b4e73c98fcda5d8964774e4242470b5
Omar D. Ramirez <omardramirez2002@gmail.com>
2022-05-22 22:28:10 -0500

    Adds logic to store new messages

--- client/src/App.js
+++ client/src/App.js
@@ -9,12 +9,31 @@ const socket = io('http://localhost:3001')
 
 function App() {
 
+    useEffect(()=>{
+      // get messsages
+    },[])
+
     const [userCode, setUserCode] = useState(null);
+    const [message, setMessage] = useState('');
+    const [sentMessages, setSentMessages] = useState([{}]);
+    const handleMessage = (event) => {
+      setMessage(event.target.value);
+    };
 
         socket.on("connect", ()=>{
             console.log("hey")
             setUserCode(socket.id)
         })
+
+    const submit = () => {
+      // store the message and change the state of the chat box to add it
+      let date = new Date()
+      let time = date.getTime();
+      setSentMessages(()=>[...sentMessages, {user:userCode,message:message,time:time}])
+      setMessage('')
+      console.log(message)
+      console.log(sentMessages)
+    }
 
 
     
@@ -25,7 +44,7 @@ function App() {
     <div className='backdrop'>
       <Paper elevation={3} sx={{minHeight: 1000, minWidth:800, display:'flex', justifyContent:"center", alignItems:'center', flexDirection:"column"}}>
         {/* name */}
-        <div>
+        <div className='header-name'>
         {
         userCode && 
         <div>{userCode}</div>
@@ -40,8 +59,10 @@ function App() {
                   label="message..."
                   variant="standard"
                   sx={{minWidth:500}}
+                  value={message}
+                  onChange={handleMessage}
         />
-              <Button variant="text">Send</Button>
+              <Button variant="text" onClick={submit}>Send</Button>
         </div>
 
         
